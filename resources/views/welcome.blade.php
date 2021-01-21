c6885c877a4d5037c0df1f642a553f32b95685ea
Renisson Silva <renisson@cea.eti.br>
2021-01-21 00:37:12 -0300

    Atualizando texto da welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Teste-Api-Github
                 </div>
 
                 <div class="links">
